0f47bf1bb167e03188a1ad83b0dc30556ad15949
Jesse Pianwi <jessepianwi007@gmail.com>
2022-09-08 18:45:38 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>                                                                                                              
 
-                                                                                                                                
+                                                                                                                            
 
 /**                                                                                                                             
 
@@ -16,8 +16,7 @@ int main(void)
 
 {                                                                                                                               
 
-        puts("\ "Programming is like building a multilingual puzzle");                                                          
-
-        return(0);                                                                                                              
+        puts("\"Programming is like building a multilingual puzzle");                                                       
+	return(0);                                                            
 
 }                   
