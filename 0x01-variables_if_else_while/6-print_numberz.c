c9c2f6df835de715489fa6bd8df39cca2f27999c
Jesse Pianwi <jessepianwi007@gmail.com>
2022-09-12 20:41:00 -0700

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,7 +6,7 @@
  * Description: Write a program that prints all
  * single digit numbers of base 10 starting
  * from 0, followed by a new line
- * Return: Always (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
