b8c8b59bc05259370b791d0386c03763ebcb4a6e
Jesse Pianwi <jessepianwi007@gmail.com>
2022-09-12 19:30:12 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,13 +3,13 @@
 /**
  * main - lowercase and uppercase
  *
- * Description: Write a program that prints the alphabet in 
+ * Description: Write a program that prints the alphabet in
  * lowercase, and then in uppercase
  * Return: Always 0 (Success)
  */
 int main(void)
-{
-	char alp;
+{	char alp;
+
 	for (alp = 'a'; alp <= 'z'; alp++)
 	{
 		putchar(alp);
