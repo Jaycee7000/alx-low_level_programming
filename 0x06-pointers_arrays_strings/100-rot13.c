e4503086059446a4c53c01f3b07ed7706df15172
Jesse Pianwi <jessepianwi007@gmail.com>
2022-09-22 17:28:29 -0700

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,7 +10,7 @@ char *rot13(char *s)
 {
 	int i, j;
 	char a[] =
-		"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMONPQRSTUVWXYZ"
+		"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	char b[] =
 	"nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
