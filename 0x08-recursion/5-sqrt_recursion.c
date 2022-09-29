7842858fccb99819f5e0dd9ace5ad7cbfb3f0359
Jesse Pianwi <jessepianwi007@gmail.com>
2022-09-28 19:38:26 -0700

    Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -14,7 +14,7 @@ int _sqrt_recursion(int n)
 }
 
 /**
- * _sqrt - returns the square root of a no
+ * _sqrt - returns the square root of a number
  * @n: test number
  * @x: squared number
  * Return: the square root of n
