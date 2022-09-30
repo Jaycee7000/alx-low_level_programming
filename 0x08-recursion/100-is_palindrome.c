c3b781bc06d91653b559ca0b9171e7c36feaefaf
Jaycee7000 <88220496+Jaycee7000@users.noreply.github.com>
2022-09-30 03:03:02 +0100

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,33 +1,24 @@
 #include "main.h"
 
-/**
- * _strlen_recursion - calculates the length of a string
- * @s: string to be used
- *
- * Return: length of the string
- */
-int _strlen_recursion(char *s)
-{
-	int sum = 0;
-
-	if (*s != '\0')
-	{
-		sum++;
-		sum += _strlen_recursion(s + 1);
-	}
-	return (sum);
-}
-
-/**
- * is_palindrome - returns the 1 if s is a palindrome
- * @s: string to be checked
- *
- * Return: 1 if s is a palindrome, 0 otherwise
- */
-int is_palindrome(char *s)
-{
-	int flag = 1;
-
-	check(s, 0, _strlen_recursion(s) - 1, &flag);
-	return (flag);
+int find_strlen(char *s);                                                                                                             
+int check_palindrome(char *s, int len, int index);                                                                                    
+int is_palindrome(char *s);                                                                                                           
+                                                                                                                                      
+/**                                                                                                                                   
+ * find_strlen - Returns the length of a string.                                                                                      
+ * @s: The string to be measured.                                                                                                     
+ *                                                                                                                                    
+ * Return: The length of the string.                                                                                                  
+ */                                                                                                                                   
+int find_strlen(char *s)                                                                                                              
+{                                                                                                                                     
+        int len = 0;                                                                                                                  
+                                                                                                                                      
+        if (*(s + len))                                                                                                               
+        {                                                                                                                             
+                len++;                                                                                                                
+                len += find_strlen(s + len);                                                                                          
+        }                                                                                                                             
+                                                                                                                                      
+        return (len);                                                                                                                 
 }
