a1679eda73e4251822a0c73ef3d9865b23f1f162
Jesse Pianwi <jessepianwi007@gmail.com>
2022-09-30 20:36:35 -0700

    Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 {
 	int num_1, num_2, mul;
 
-	for (argc != 3)
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
@@ -22,6 +22,5 @@ int main(int argc, char *argv[])
 	num_2 = atoi(argv[2]);
 	mul = num_1 * num_2;
 	printf("%d\n", mul);
-	
 	return (0);
 }
