111f79e4164bc2a818185b0e3dcd5375dcb3d990
Jaycee7000 <88220496+Jaycee7000@users.noreply.github.com>
2022-10-05 03:34:19 +0100

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,22 +16,20 @@ char *str_concat(char *s1, char *s2)
 	int index, concat_index = 0, len = 0;
 
 	if (s1 == NULL)
-		s1 = "";
-
+	s1 = "";
+	
 	if (s2 == NULL)
-		s2 = "";
+	s2 = "";
 	for (index = 0; s1[index] || s2[index]; index++)
-		len++;
+	len++;
 	concat_str = malloc(sizeof(char) * len);
-
+	
 	if (concat_str == NULL)
-		return (NULL);
-
+	return (NULL);
 	for (index = 0; s1[index]; index++)
-		concat_str[concat_index++] = s1[index];
-
+	concat_str[concat_index++] = s1[index];
+	
 	for (index = 0; s2[index]; index++)
-		concat_str[concat_index++] = s2[index];
-
+	concat_str[concat_index++] = s2[index];
 	return (concat_str);
 }
