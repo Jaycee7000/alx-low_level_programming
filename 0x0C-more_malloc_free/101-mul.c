e6d58570304ae3d680442038d3df554d33237002
Jesse Pianwi <jessepianwi007@gmail.com>
2022-10-07 16:28:00 -0700

    We must accept finite disappointment, but never lose infinite hope

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,7 @@
+ gjt j jv tj vjl j r fn rb 4b fnb4br4 mnvkfjbn fb
+ dj42 dh2 hr rb42 rb rv42rb2nr2r
+ fvjk h4 h 5b5t  t,ht h5t5t
+ f4kjbt5tn5 tn t 5t
+ fwj4bbr4tbtjkt
+ 4lbrj4rj4.334jtbj4t j t t
+ 3kfj3fnk3n3k t3j t
