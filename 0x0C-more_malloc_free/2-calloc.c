aebb74497554b135784c7bd157a8efea78099f7e
Jesse Pianwi <jessepianwi007@gmail.com>
2022-10-07 16:42:44 -0700

    _calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,6 @@
+feherhfbhf h5f 5jl53bj35bfkj3f kf5 3fj3
+rjbfjk3b4jf43 bf 34nmf43 n j fj4 ff jf 
+f4jbfb4jbj4bnfb4fjbf4h43h v4g3 gf
+flj4fbj3 4fh34 f34flkb34j f43k f34 
+f4j3 b4fj3 fkf 4h 43kfj4lkf43f34
+4fj3fbj3bfj3
