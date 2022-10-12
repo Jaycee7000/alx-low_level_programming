ca6b0c7e212106e8e564e7f41541b9bc0d02e807
Jaycee7000 <88220496+Jaycee7000@users.noreply.github.com>
2022-10-12 01:18:30 +0100

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,12 +2,11 @@
 #define DOG_H
 
 /**
- * struct dog - A type describing a dog.
+ * struct dog - A new type describing a dog.
  * @name: The name of the dog.
  * @age: The age of the dog.
  * @owner: The owner of the dog.
  */
-
 struct dog
 {
 	char *name;
@@ -18,7 +17,6 @@ struct dog
 /**
  * dog_t - Typedef for struct dog
  */
-
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
